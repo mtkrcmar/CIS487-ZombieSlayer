8d3fd645c2541d19525f5ad0d15bcfa1fe37bd05
AndrewRodman <rodman@umich.edu>
2016-12-17 14:30:37 -0500

    Changes I made
    added a feature where the screen will blink red when taking damage. Also
    I added a zombie kill count on the gui and a kill streak text pop up
    every time the player gets 5 kills.

---
+++ Assets/Scripts/ZombieKillCount.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieKillCount : MonoBehaviour {
+	public static int zombieKillCount =0;
+	public UnityEngine.UI.Text zombieKillsText;
+	public UnityEngine.UI.Text zombieKillStreak;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		zombieKillsText.text="Zombie Kills: "+zombieKillCount;
+		if (zombieKillCount%5==0&&zombieKillCount!=0) // every 5 kills
+		{
+			StartCoroutine (zombieKillStreakText());
+		}
+	}
+
+	IEnumerator zombieKillStreakText()
+	{
+		Debug.Log ("Kill Streak");
+		zombieKillStreak.text="Kill Streak!!!";
+		zombieKillStreak.gameObject.SetActive (true);
+		yield return new WaitForSeconds(3.5f);
+		zombieKillStreak.gameObject.SetActive (false);
+	}
+}
